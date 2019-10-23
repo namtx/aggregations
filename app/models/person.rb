5608d45be546a79b37780d79b4e0432afcb77cd9
Tran Xuan Nam <namtx@Trans-MacBook-Pro.local>
2019-10-23 18:17:20 +0700

    Finish the exercise

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,14 +1,23 @@
+# frozen_string_literal: true
+
 class Person < ActiveRecord::Base
   belongs_to :location
   belongs_to :role
-  belongs_to :manager, class_name: "Person", foreign_key: :manager_id
-  has_many :employees, class_name: "Person", foreign_key: :manager_id
+  belongs_to :manager, class_name: 'Person', foreign_key: :manager_id
+  has_many :employees, class_name: 'Person', foreign_key: :manager_id
 
   def self.maximum_salary_by_location
-    {}
+    group(:location_id).maximum(:salary)
   end
 
   def self.managers_by_average_salary_difference
-    all
+    require 'pry'
+    salaries = select('manager_id, AVG(salary) avg_salary')
+               .group(:manager_id)
+               .where('manager_id IS NOT NULL')
+               .to_sql
+
+    joins("INNER JOIN (#{salaries}) salaries ON salaries.manager_id = people.id")
+      .order('people.salary - salaries.avg_salary DESC')
   end
 end
